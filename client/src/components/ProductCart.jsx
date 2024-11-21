7526ce5d6af0163b0f549c25b420c89dd6aaec51
wawk <white_knight@prodigy.net>
2024-11-21 07:40:38 -0500

    Added screens and ProductsScreen component using Chakra

---
+++ client/src/components/ProductCart.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const ProductCart = () => {
+  return (
+    <div>ProductCart</div>
+  )
+}
+
+export default ProductCart
---
+++ client/src/screens/ProductsScreen.js
@@ -0,0 +1,14 @@
+import {Box} from '@chakra-ui/react'
+import ProductCart from '../components/ProductCart';
+
+const ProductsScreen = () => {
+
+    return  (
+       <Box>
+        Chakra Box
+       </Box>
+            
+    );
+};
+
+export default ProductsScreen
